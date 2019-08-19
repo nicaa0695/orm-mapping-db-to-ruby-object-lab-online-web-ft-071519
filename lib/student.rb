a0c268797bcac3158b7a43c3765d8c3e31f3fc64
Veronica Lopez <nicaa0695@gmail.com>
2019-08-19 17:48:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
 
   def self.all
     sql = "SELECT * FROM students"
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
     end
   end
@@ -26,7 +26,29 @@ class Student
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first 
-
+  end
+  
+  def self.students_below_12th_grade 
+    sql = <<-SQL
+    SELECT * 
+    FROM students 
+    WHERE students.grade < 12 
+    SQL
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  def self.first_student_in_grade_10 
+    sql = <<-SQL
+    SELECT * 
+    FROM students 
+    WHERE students.grade = 10 
+    LIMIT 1 
+    SQL
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end.first 
   end
   
   def save
